667da8166148d86807db3d2d9cd11ae8f1358f9b
DriesCode <adri7escalona@gmail.com>
2019-04-20 01:20:39 +0200

    Finalizado programa prueba GPS Logger con SD Card
    Finalizado el programa cuya función es registrar los valores del GPS y guardarlos en un archivo LOG.TXT en una tarjeta microSD externa.

--- test_gps_datalogger/test_gps_datalogger.ino
+++ test_gps_datalogger/test_gps_datalogger.ino
@@ -3,27 +3,56 @@
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
 
-File gpsFile;
-SoftwareSerial gps(4,3);
-TinyGPSPlus tGPS;
+File dataFile; // Archivo logs donde guardaremos los datos
+SoftwareSerial ss(4,3); // Conexión con el módulo GPS
+TinyGPSPlus gps; // Objeto GPS que nos permitirá la abstracción
+
+String zero = "0"; // Definir un String "zero" para su posterior uso formateando la hora
+
+double lati, longi; // Variables que guardarán los valores de longitud y latitud
 
 void setup() {
-  Serial.begin(9600);
-  gps.begin(9600);
+  Serial.begin(9600); // Iniciar seriales en 9600 baud/s
+  ss.begin(9600);
 
-  while (!Serial) { ; }
+  while (!Serial) { ; } // Esperar a que se inicie el serial
 
-  SD.begin();
+  if (!SD.begin()) { Serial.println("Error iniciando la tarjeta SD"); while (1) { ; } } // Si hay un problema iniciando la tarjeta SD, no ejecutar siguiente código
 }
 
 void loop() {
-  while (gps.available()) {
-    tGPS.encode(gps.read());
-    if (tGPS.location.isUpdated()) {
-      Serial.print("Latitude= "); 
-      Serial.print(tGPS.location.lat(), 6);
-      Serial.print(" Longitude= "); 
-      Serial.println(tGPS.location.lng(), 6);
-    }
-  }
+  zero = "0"; // En cada ciclo del programa, resetear la cadena "zero"
+  while (ss.available() > 0) gps.encode(ss.read()); // Abastecer al objeto gps con datos del sensor GPS
+
+  if (!gps.location.isUpdated()) return; // Si los datos no están actualizados, no ejecutar siguiente código
+
+  lati = (double) gps.location.lat(); // Actual latitud GPS
+  longi = (double) gps.location.lng(); // Actual longitud GPS
+  
+  // Tiempo de la captación de datos
+  uint8_t hora = gps.time.hour(); 
+  uint8_t minuto = gps.time.minute();
+  uint8_t segundo = gps.time.second();
+
+  // Si el número (hora/minuto/segundo) tiene solo un dígito, añadirle un cero a la izquierda
+  String horaS = ((hora < 10 && hora != 0) ? zero += hora : String(hora));
+  String minutoS = ((minuto < 10 && minuto != 0) ? zero += minuto : String(minuto));
+  String segundoS = ((segundo < 10 && segundo != 0) ? zero += segundo : String(segundo));
+
+  dataFile = SD.open("logs.txt", FILE_WRITE); // Abrir el archivo de logs para escribir en él
+
+  if (!dataFile) { Serial.println("Error abriendo el archivo"); while (1) { ; } } // Si hay un problema abriendo el archivo log, no ejecutar siguiente código
+
+  // Partes de la cadena final que se imprimirá en el archivo logs
+  String tiempo = String("[" + horaS + ":" + minutoS + ":" + segundoS + "] ");
+  String latitud = String("Latitud: ");
+  String longitud = String(" || Longitud: ");
+
+  // Imprimir la información al archivo logs
+  dataFile.print(tiempo);
+  dataFile.print(latitud); dataFile.print(lati, 6); dataFile.print(longitud); dataFile.print(longi, 6); dataFile.println();
+  dataFile.close();
+  
+  delay(10000); // Guardar los datos cada 10 segundos
+
 }
