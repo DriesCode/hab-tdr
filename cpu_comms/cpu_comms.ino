009641e260a61ebd7a46e54ec1fe82ce8910ea3c
DriesCode <adri7escalona@gmail.com>
2019-04-21 23:23:51 +0200

    Comentado el código de CPU de comunicaciones
    Se ha comentado todo el código actual del Arduino destinado a la comunicación de los datos de los sensores y el GPS

--- cpu_comms/cpu_comms.ino
+++ cpu_comms/cpu_comms.ino
@@ -5,72 +5,63 @@
 #include <TinyGPS++.h>
 #include <Wire.h>
 
-#define RX_PIN 7
-#define TX_PIN 8
+#define RX_PIN 7 // Pin recibir datos
+#define TX_PIN 8 // Pin para enviar datos ( No usado )
 
-#define GPS_RX_PIN 3
-#define GPS_TX_PIN 4
+#define GPS_RX_PIN 3 // Pin recibir datos GPS
+#define GPS_TX_PIN 4 // Pin enviar datos GPS ( No usado )
 
-File logs;
-SoftwareSerial arduino1 (RX_PIN, TX_PIN);
-SoftwareSerial ssGPS (GPS_RX_PIN, GPS_TX_PIN);
-TinyGPSPlus gps;
+File logs; // Archivo donde guardar logs
+SoftwareSerial arduino1 (RX_PIN, TX_PIN); // Conexión con el otro arduino
+SoftwareSerial ssGPS (GPS_RX_PIN, GPS_TX_PIN); // Conexión con el GPS
+TinyGPSPlus gps; // Objeto que nos permitirá la abstracción
 
-String zero = "0";
+String zero = "0"; // String necesario para formatear la hora
 
-double lati, longi;
-float temperatura, presion, humedad, altitud;
-uint8_t hora, minuto, segundo;
-String horaStr, minutoStr, segundoStr, tiempo, latiStr, longiStr;
+double lati, longi; // Variables donde se guardarán la latitud y longitud
+float temperatura, presion, humedad, altitud; // Variables donde se guardarán los datos de los sensores
+uint8_t hora, minuto, segundo; // Variables donde se guardarán los valores numéricos del tiempo
+String horaStr, minutoStr, segundoStr, tiempo, latiStr, longiStr; // Variables donde se guardarán las cadenas de texto que se imprimirán en el archivo logs
 
-String datos;
+String datos; // Cadena de datos que se reciben del otro arduino
 
 void setup() {
-  Serial.begin(9600);
-  arduino1.begin(9600);
-  ssGPS.begin(9600);
+  Serial.begin(9600); // Iniciar comunicación serial con el ordenador
+  arduino1.begin(9600); // Iniciar comunicación serial con el otro arduino
+  ssGPS.begin(9600); // Iniciar comunicación serial con el GPS
   
-
-  //while (!arduino1) { ; }
-  //Serial.println("arduino 1 conectado");
-  //if (!SD.begin()) { Serial.println("Error iniciando la tarjeta SD"); while (1) { ; } }
+  if (!SD.begin()) { Serial.println("Error iniciando la tarjeta SD"); while (1) { ; } } // Iniciar el lector de la tarjeta SD. Si no se inicia, no ejecutar siguiente código
 }
 
 void loop() {
-  datos = "";
-  arduino1.listen();
+  datos = ""; // Reiniciar la cadena de datos en cada iteración del programa
+  arduino1.listen(); // Escuchar el puerto del arduino
 
-  if (arduino1.isListening()) {
+  if (arduino1.isListening()) { // Asegurarnos de que se está escuchando el puerto correcto
     Serial.println("Listenign arduino");
-    while (arduino1.available() > 0) {
-      char c = arduino1.read();
-      if (c != "-1") datos += c;
-  }
-
+    while (arduino1.available() > 0) { // Ejecutar mientras recibamos datos del arduino
+      char c = arduino1.read(); // Guardar el byte que hemos recibido
+      if (c != "-1") datos += c; // Si es diferente a '-1', añadirlo a la cadena de texto
+    }
+    
     delay(1500);
-  
     Serial.println(datos);
   }
-  
-  
 
-  ssGPS.listen();
+  ssGPS.listen(); // Escuchar el puerto del GPS
 
-  if (ssGPS.isListening()) {
+  if (ssGPS.isListening()) { // Asegurarnos de que se está escuchando el puerto correcto
     Serial.println("listening gps");
-    while (ssGPS.available() > 0) gps.encode(ssGPS.read());
+    while (ssGPS.available() > 0) gps.encode(ssGPS.read()); // Preparar los datos del GPS para poder manipularlos con el objeto TinyGPSPlus
 
-    if (!gps.time.isUpdated()) return;
+    if (!gps.time.isUpdated()) return; // Si los datos de tiempo no están actualizados, no ejecutar siguiente código
     
-    getTime(&gps);
+    getTime(&gps); // Modificar variables que almacenan el valor del tiempo
     
     Serial.println(tiempo);
     
     delay(1500);
   }
-
-  
-  
 }
 
 void getArduinoSensorData(SoftwareSerial* arduino1) {
@@ -80,25 +71,29 @@ void getArduinoSensorData(SoftwareSerial* arduino1) {
 }
 
 void getLocation(TinyGPSPlus* gps) {
-  lati = gps->location.lat();
-  longi = gps->location.lng();
+  lati = gps->location.lat(); // Obtener la latitud actual
+  longi = gps->location.lng(); // Obtener la longitud actual
 }
 
 void getTime(TinyGPSPlus* gps) {
-  hora = gps->time.hour();
-  minuto = gps->time.minute();
-  segundo = gps->time.second();
+  hora = gps->time.hour(); // Obtener la hora actual
+  minuto = gps->time.minute(); // Obtener el minuto actual
+  segundo = gps->time.second(); // Obtener el segundo actual
 
-  horaStr = ((hora < 10 && hora != 0) ? zero += hora : String(hora));
+  // Si cualquier valor (minuto/segundo/hora) solo tiene un dígito, añadirle un cero a la izquierda
+  
+  horaStr = ((hora < 10 && hora != 0) ? zero += hora : String(hora)); 
   zero = "0";
   minutoStr = ((minuto < 10 && minuto != 0) ? zero += minuto : String(minuto));
   zero = "0";
   segundoStr = ((segundo < 10 && segundo != 0) ? zero += segundo : String(segundo));
   zero = "0";
 
+  // Si cualquier valor es cero, añadir otro
+
   horaStr = ((hora == 0) ? String("00") : String(horaStr));
   minutoStr = ((minuto == 0) ? String("00") : String(minutoStr));
   segundoStr = ((segundo == 0) ? String("00") : String(segundoStr));
 
-  tiempo = String("[" + horaStr + ":" + minutoStr + ":" + segundoStr + "] ");
+  tiempo = String("[" + horaStr + ":" + minutoStr + ":" + segundoStr + "] "); // Agrupar los datos en una cadena de texto conjunta
 }
