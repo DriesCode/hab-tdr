c55b5000033354f4ae3cb12b6a797be2a264b21d
DriesCode <adri7escalona@gmail.com>
2019-09-01 15:38:31 +0200

    Comentado las líneas de código original del ESP8266
    Se ha comentado cada línea de código original del programa del ESP8266

--- cpu_esp8266/cpu_esp8266.ino
+++ cpu_esp8266/cpu_esp8266.ino
@@ -1,22 +1,18 @@
-/**
-   BasicHTTPClient.ino
-
-    Created on: 24.05.2015
-
-*/
-
 #include <Arduino.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
 #include <ESP8266HTTPClient.h>
-
 #include <WiFiClient.h>
 
 ESP8266WiFiMulti WiFiMulti;
 
-bool alreadyConnected = false;
+bool alreadyConnected = false; // Marcar si estamos ya conectados o no
+
+String data[6]; // Crear un array de String de 6 espacios que contendrá cada dato recibido
+String token = "&field"; // Token que se añadirá por cada variable a la petición GET
+
+bool dataToSend = false; // Variable que marcará si estamos listos para enviar datos
+bool gotValues = false; // Variable que marcará si hemos recibido datos
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -34,48 +30,43 @@ void setup() {
   }
 
   WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("MiAdri", "adrian2002grison");
-
-  alreadyConnected = true;
+  
+  WiFiMulti.addAP("red", "contraseña"); // Conectarse a la red wifi (en este caso, portatil del móvil)
+  alreadyConnected = true; // Marcar que ya nos hemos conectado a la red
 }
-
-String data[6];
-String token = "&field";
-
-bool dataToSend = false;
-bool gotValues = false;
 
 WiFiClient client;
 HTTPClient http;
 
 void loop() {
-  String url = "http://api.thingspeak.com/update?api_key=M5Z4I1ZCB8ZD12UI";
-  if (Serial.available() > 0 && !dataToSend) {
-    String d = Serial.readString();
-    Serial.println("Got: " + d);
-    char d_c[d.length()];
+  String url = "http://api.thingspeak.com/update?api_key=M5Z4I1ZCB8ZD12UI"; // URL base que se le añadirá cada token con variable
+  if (Serial.available() > 0 && !dataToSend) { // Si tenemos datos en Serial y no tenemos datos para enviar por WIFI, continuar
+    String d = Serial.readString(); // Almacenar en variable la cadena de texto que recibimos
+    Serial.println("Got: " + d); // Imprimir por pantalla que hemos recibido datos (debugging)
+    char d_c[d.length()]; // Crear la varibale d_c con un datos en forma de array de caracteres
 
-    d.toCharArray(d_c, sizeof(d_c));
+    d.toCharArray(d_c, sizeof(d_c)); // Creación propiamente del array d_c
     
-    char* nextValue;
-    nextValue = strtok(d_c, ",");
-    int i = 0;
-    while (nextValue != NULL) {
-      Serial.print("Split " + String(i) + ": ");
+    char* nextValue; // Variable que guardará el caracter siguiente para la conversión
+    nextValue = strtok(d_c, ","); // Asignarle el valor al primer caracter al romper la cadena por comas
+    int i = 0; // Posición del valor
+    while (nextValue != NULL) { // Repetir mientras hasta que no haya más caracteres
+      Serial.print("Split " + String(i) + ": "); // Mostrar información del caracter actual (debugging)
       Serial.println(nextValue);
-      data[i] = nextValue;
-      nextValue = strtok(NULL, ",");
-      i++;
+      data[i] = nextValue; // Asignar el caracter al array data en la misma posición que tenía en d_c
+      nextValue = strtok(NULL, ","); // Reasignar nextValue al siguiente valor
+      i++; // Incrementar por uno "i"
     }
 
-    dataToSend = true;
+    dataToSend = true; // Marcar que ya hemos recibido datos y tenemos datos por enviar
     gotValues = true;
   }
 
-  if (dataToSend && gotValues) {
-    Serial.println("Sending data...");
-    if ((WiFiMulti.run() == WL_CONNECTED)) {
-      Serial.println("Creating URL for GET request...");
+  if (dataToSend && gotValues) { // Continuar si se cumplen las dos condiciones anteriores
+    Serial.println("Sending data..."); // Debugging
+    if ((WiFiMulti.run() == WL_CONNECTED)) { // Comprobar si estamos conectados al WIFI
+      Serial.println("Creating URL for GET request..."); // Debugging
+      // Modificar la URL base para que contenga todos los valores de cada variable
       for (int i = 1; i < 7; i++) {
         url.concat(token);
         url.concat(i);
@@ -83,7 +74,7 @@ void loop() {
         url.concat(data[(i-1)]);
       }
 
-      Serial.println("Final URL: " + url);
+      Serial.println("Final URL: " + url); // Debugging
 
       Serial.print("[HTTP] begin...\n");
       if (http.begin(client, url)) {  // HTTP
@@ -100,13 +91,15 @@ void loop() {
   
           // file found at server
           if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-            String payload = http.getString();
-            Serial.println(payload);
-            if (payload != "0") {
-              dataToSend = false;
+            
+            String payload = http.getString(); // Recibir la respuesta del servidor
+            Serial.println(payload); // Debugging
+            if (payload != "0") { // Si la respuesta del servidor es diferente a 0, continuar
+              dataToSend = false; // Restablecer variables de estado
               gotValues = false;
-              delay(500);
+              delay(500); // Esperar 500 ms para no sobrecargar
             }
+            
           }
         } else {
           Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
